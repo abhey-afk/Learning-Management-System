63d4d27873699c3dc301559c9572480c11bafa13
abhey-afk <ab852759@gmail.com>
2025-05-29 20:34:51 +0530

    fix: Update health route to use ES modules syntax

--- server/routes/health.route.js
+++ server/routes/health.route.js
@@ -1,8 +1,8 @@
-const express = require('express');
+import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.status(200).json({ status: 'ok', message: 'Server is healthy' });
 });
 
-module.exports = router; 
+export default router; 
