7e8cc86aed8a35b155fc2367f4bd1ef7e98a7e4c
abhey-afk <ab852759@gmail.com>
2025-05-29 21:58:34 +0530

    fix: remove deprecated MongoDB connection options

--- server/database/db.js
+++ server/database/db.js
@@ -10,8 +10,6 @@ const connectDB = async () => {
         console.log('Attempting to connect to MongoDB...');
         
         const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
             serverSelectionTimeoutMS: 30000,
             socketTimeoutMS: 30000,
         });
